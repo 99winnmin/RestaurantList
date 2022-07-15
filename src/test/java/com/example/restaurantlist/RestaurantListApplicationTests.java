4f7b88244a5b6ea867b275c4ba226646b189a804
seungmin Ryu <ojysep9987@naver.com>
2022-07-15 22:54:49 +0900

    Project Initial Setting

---
+++ src/test/java/com/example/restaurantlist/RestaurantListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
